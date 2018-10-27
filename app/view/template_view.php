95adbac8e6c90eab69ec2394bbc9326a936a8332
Unknown <anoque@mail.ru>
2018-10-28 02:21:38 +0700

    Delete material design and add materialize

--- app/view/template_view.php
+++ app/view/template_view.php
@@ -5,45 +5,29 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
-	<!-- Get material design -->
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-	<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- Materialize -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="/js/main.js"></script>
 	
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
 </head>
 <body>
-	<!-- Always shows a header, even in smaller screens. -->
-	<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-		<header class="mdl-layout__header">
-			<div class="mdl-layout__header-row">
-				<!-- Title -->
-				<span class="mdl-layout-title"><?=$data['title']?></span>
-				<!-- Add spacer, to align navigation to the right -->
-				<div class="mdl-layout-spacer"></div>
-				<!-- Navigation. We hide it in small screens. -->
-				<nav class="mdl-navigation mdl-layout--large-screen-only">
-					<a class="mdl-navigation__link" href="/">Main</a>
-					<a class="mdl-navigation__link" href="/tree/add">Add</a>
-				</nav>
-			</div>
-		</header>
-		<!-- <div class="mdl-layout__drawer">
-		<span class="mdl-layout-title">Title</span>
-			<nav class="mdl-navigation">
-				<a class="mdl-navigation__link" href="">222</a>
-				<a class="mdl-navigation__link" href="">Link</a>
-				<a class="mdl-navigation__link" href="">Link</a>
-				<a class="mdl-navigation__link" href="">Link</a>
-			</nav>
-		</div> -->
-		<main class="mdl-layout__content">
-			<div class="page-content"><?php include_once 'app/view/'.$content_view;?></div>
-		</main>
+	<nav>
+		<div class="nav-wrapper">
+			<a href="#" class="brand-logo"><?= $data['title'] ?></a>
+			<ul id="nav-mobile" class="right hide-on-med-and-down">
+				<li><a href="/">Main page</a></li>
+				<li><a href="/tree/add">Add member</a></li>
+				<li><a href="/tree/list">Members list</a></li>
+			</ul>
+		</div>
+	</nav>
+
+	<div class="container">
+		<?php include_once 'app/view/'.$content_view;?>
 	</div>
-	<!-- <div id="content">
-		
-	</div> -->
 </body>
 </html>
