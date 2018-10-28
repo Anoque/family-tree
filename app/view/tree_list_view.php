9dcb4dee5b6554be45beb151e7c6443d5a15b59d
Unknown <anoque@mail.ru>
2018-10-28 11:26:16 +0700

    Change collection to the table in tree list

--- app/view/tree_list_view.php
+++ app/view/tree_list_view.php
@@ -6,16 +6,27 @@
 <? if (isset($data['delete'])): ?>
     <h3><?= ($data['delete']) ? "Deleted" : "Error" ?></h3>
 <? else: ?>
-    <ul class="collection with-header">
-        <li class="collection-header"><h4>Names</h4></li>
-        <? foreach($data['members'] as $item): ?>
-            <li class="collection-item">
-                <div>
-                    <?= $item['name'] ?>
-                    <a href="/tree/delete/<?= $item['id'] ?>" class="secondary-content"><i class="material-icons" title="Delete">delete</i></a>
-                    <a href="/tree/add/<?= $item['id'] ?>" class="secondary-content"><i class="material-icons" title="Edit">account_box</i></a>
-                </div>
-            </li>
-        <? endforeach; ?>
-    </ul>
+    <table>
+        <thead>
+            <th>Names</th>
+            <th>Sex</th>
+            <th>Actions</th>
+        </thead>
+        <tbody>
+            <? foreach($data['members'] as $item): ?>
+                <tr>
+                    <td>
+                        <span><?= $item['name'] ?></span>
+                    </td>
+                    <td>
+                        <span style="color: #<?= ($item['sex'] == 0) ? "4169E1" : "F08080" ?> "><?= ($item['sex'] == 0) ? "Male" : "Female" ?></span>
+                    </td>
+                    <td>
+                        <a href="/tree/delete/<?= $item['id'] ?>" class="secondary-content"><i class="material-icons" title="Delete">delete</i></a>
+                        <a href="/tree/add/<?= $item['id'] ?>" class="secondary-content"><i class="material-icons" title="Edit">account_box</i></a>
+                    </td>
+                </tr>
+            <? endforeach; ?>
+        </tbody>
+    </table>
 <? endif; ?>
